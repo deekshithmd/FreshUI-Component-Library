de2d8a48167d74768e8faa8502083f395f0a5529
Deekshith M D <deekshithmogra@gmail.com>
2022-02-20 21:38:48 +0530

    refactor: dark theme functionality update

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const dark=document.querySelector(".dark");
 
+if(localStorage.getItem("dark")==="dark-theme"){
+    document.body.classList.add("dark-theme")
+    dark.classList.remove("fa-sun")
+    dark.classList.add("fa-moon")
+}
+else{
+    document.body.classList.add("light-theme")
+}
+
 dark.addEventListener('click',()=>{
     if(dark.classList.contains("fa-sun")){
+        localStorage.clear();
+        localStorage.setItem("dark","dark-theme")
         dark.classList.remove("fa-sun")
         dark.classList.add("fa-moon")
         document.body.classList.remove("light-theme")
         document.body.classList.add("dark-theme")
     }
     else{
+        localStorage.clear();
+        localStorage.setItem("dark","light-theme")
         dark.classList.remove("fa-moon")
         dark.classList.add("fa-sun")
         document.body.classList.remove("dark-theme")
